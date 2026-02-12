2b300afc0a4c95a79909d58a8aa8441c7c90885d
苏奕霖 <82499150+2766733506@users.noreply.github.com>
2026-02-12 13:37:31 +0800

    Add files via upload

---
+++ tool/locla_cam.py
@@ -0,0 +1,14 @@
+import math
+
+import torch.nn as nn
+import random
+import numpy as np
+from torch.nn.grad import conv2d_weight
+import torch.nn.functional as F
+import torch
+
+
+
+class HookGradCam:
+    # 预印本不提供TIP具体实现
+    pass
